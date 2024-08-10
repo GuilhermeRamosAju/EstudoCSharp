9021177ec9b7d42eba6c69eebb694da010df3222
Guilherme Ramos <105815487+GuilhermeRamosAju@users.noreply.github.com>
2024-08-10 10:42:18 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            Console.WriteLine(p);
+
+            p.SetNome("TV 4K");
+
+            Console.WriteLine(p.GetNome());
+        }
+    }
+}
